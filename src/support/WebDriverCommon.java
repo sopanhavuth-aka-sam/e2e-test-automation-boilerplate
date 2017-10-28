14be020b2a0564b82961f848d81bec84bcc0b9e8
Sopanhavuth-aka-sam <sopanhavuth.som@gmail.com>
2017-10-28 12:43:54 -0700

    Save and moving this project to eclipse

---
+++ src/support/WebDriverCommon.java
@@ -0,0 +1,10 @@
+public class WebDriverCommon {
+    //this is where the commonly used function live: click, scroll to
+    public WebDriverCommon() {
+        Webdriver driver = new ChromeDriver();
+    }
+
+    public void openurl() {
+
+    }
+}
